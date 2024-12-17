6040d0614d8188419be759ee95ef461b36ef9953
Winrey <i@iaside.com>
2024-12-17 17:51:26 +0800

    feat: add claude scan

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,50 @@
 import { Command } from 'commander';
+import { ClaudeHostService } from './services/claude';
 
 const packageJson = require('../package.json');
 const version: string = packageJson.version;
 
 const program = new Command();
 
+const claudeSrv = new ClaudeHostService();
+
 program
   .version(version)
-  .name('my-command')
-  .option('-d, --debug', 'enables verbose logging', false)
-  .parse(process.argv);
+  .name('mcpm')
+  .option('-d, --debug', 'enables verbose logging', false);
 
-// Function code for CLI goes here
+// program
+//   .command('install')
+//   .description('Install a new MCP project')
+//   .action(() => {
+//     console.log('Install a new MCP project');
+//   });
+
+program
+  .command('add')
+  .description('Add a new MCP host')
+  .action(() => {
+    console.log('Add a new MCP host');
+  });
+
+const hostCmd = program
+  .command('host')
+  .description('Manage your MCP hosts like Claude App');
+
+hostCmd
+  .command('scan')
+  .description('Scan for supported hosts')
+  .action(() => {
+    console.log('Scanning Claude');
+
+    claudeSrv
+      .listMCPServers()
+      .then(config => {
+        console.log(JSON.stringify(config, null, 2));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
+
+program.parse(process.argv);
---
+++ src/services/claude.ts
@@ -0,0 +1,156 @@
+import path from 'path';
+import os from 'os';
+import { promises as fsp } from 'fs';
+
+export interface MCPServer {
+  command: string;
+  args: string[];
+}
+
+export interface ClaudeConfig {
+  mcpServers?: {
+    [key: string]: MCPServer;
+  };
+}
+
+export class ClaudeFileService {
+  constructor(private readonly fs: typeof fsp = fsp) {}
+
+  async getClaudeConfig(): Promise<ClaudeConfig | null> {
+    try {
+      const configPath = ClaudeFileService.getClaudeConfigPath();
+      if (!configPath) return null;
+      const content = await this.fs.readFile(configPath, 'utf8');
+      return JSON.parse(content) as ClaudeConfig;
+    } catch {
+      console.log('Claude config not found');
+      return null;
+    }
+  }
+
+  private createDefaultClaudeConfig(): ClaudeConfig {
+    return {
+      mcpServers: {},
+    };
+  }
+
+  private stringifyClaudeConfig(config: ClaudeConfig): string {
+    return JSON.stringify(config, null, 2);
+  }
+
+  async createClaudeConfigFile(): Promise<ClaudeConfig> {
+    const defaultConfig = this.createDefaultClaudeConfig();
+    const configPath = ClaudeFileService.getClaudeConfigPath();
+    if (!configPath) throw new Error('Unsupported platform');
+    await this.fs.writeFile(
+      configPath,
+      this.stringifyClaudeConfig(defaultConfig)
+    );
+    console.log('Claude config created');
+    return defaultConfig;
+  }
+
+  async getOrCreateClaudeConfig(): Promise<ClaudeConfig> {
+    const content = await this.getClaudeConfig();
+    if (content) {
+      return content;
+    }
+    return await this.createClaudeConfigFile();
+  }
+
+  async writeClaudeConfigFile(config: ClaudeConfig): Promise<void> {
+    const configPath = ClaudeFileService.getClaudeConfigPath();
+    if (!configPath) throw new Error('Unsupported platform');
+    await this.fs.writeFile(configPath, this.stringifyClaudeConfig(config));
+  }
+
+  async modifyClaudeConfigFile(
+    modifier: (config: ClaudeConfig) => Promise<ClaudeConfig> | ClaudeConfig
+  ): Promise<ClaudeConfig> {
+    const config = await this.getOrCreateClaudeConfig();
+    const newConfig = await modifier(config);
+    await this.writeClaudeConfigFile(newConfig);
+    return newConfig;
+  }
+
+  static getClaudeConfigPath(): string {
+    // process.platform Posible values：
+    // 'aix' - IBM AIX
+    // 'darwin' - macOS
+    // 'freebsd' - FreeBSD
+    // 'linux' - Linux
+    // 'openbsd' - OpenBSD
+    // 'sunos' - SunOS
+    // 'win32' - Windows(32-bit or 64-bit)
+
+    const home = os.homedir();
+
+    if (process.platform === 'win32') {
+      // Windows Path
+      return path.join(
+        home,
+        'AppData',
+        'Local',
+        'Claude',
+        'claude_desktop_config.json'
+      );
+    } else if (process.platform === 'darwin') {
+      // macOS Path
+      return path.join(
+        home,
+        'Library',
+        'Application Support',
+        'Claude',
+        'claude_desktop_config.json'
+      );
+    } else {
+      // Linux/Unix Path
+      return path.join(home, '.config', 'claude', 'claude_desktop_config.json');
+    }
+  }
+}
+
+export class ClaudeHostService {
+  constructor(
+    public readonly fileSrv: ClaudeFileService = new ClaudeFileService()
+  ) {}
+
+  private addMCPServerToConfig(
+    config: ClaudeConfig,
+    name: string,
+    server: MCPServer
+  ): ClaudeConfig {
+    config.mcpServers = config.mcpServers || {};
+    config.mcpServers[name] = server;
+    return config;
+  }
+
+  private removeMCPServerFromConfig(
+    config: ClaudeConfig,
+    serverName: string
+  ): ClaudeConfig {
+    config.mcpServers = config.mcpServers || {};
+    if (!config.mcpServers[serverName]) {
+      throw new Error(`Server ${serverName} not found`);
+    }
+    delete config.mcpServers[serverName];
+    return config;
+  }
+
+  async addMCPServer(name: string, server: MCPServer): Promise<ClaudeConfig> {
+    return await this.fileSrv.modifyClaudeConfigFile(config =>
+      this.addMCPServerToConfig(config, name, server)
+    );
+  }
+
+  async removeMCPServer(serverName: string): Promise<ClaudeConfig> {
+    return await this.fileSrv.modifyClaudeConfigFile(config =>
+      this.removeMCPServerFromConfig(config, serverName)
+    );
+  }
+
+  async listMCPServers(): Promise<MCPServer[]> {
+    const config = await this.fileSrv.getClaudeConfig();
+    return Object.values(config?.mcpServers || {});
+  }
+}
